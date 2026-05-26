d9b801cd22f464622e5a7b093c7932c39f75e57a
BLOOD212 <g95829914@gmail.com>
2026-05-26 09:44:22 +0200

    Update config.js

--- config.js
+++ config.js
@@ -16,9 +16,9 @@ const moduleCache = new NodeCache({ stdTTL: 300 });
 global.sam = ['393701330693'];
 global.owner = [
   ['393701330693', 'blood', true],
-  ['79384688438', 'athena', true],
-  ['393925966353', 'vixie', true],
-  ['393780306700', 'deadlu', true],
+  ['xxxxxxxxxxxx', 'xxxxx', true],
+  ['xxxxxxxxxxxx', 'xxxxx', true],
+  ['xxxxxxxxxxxx', 'xxxxx', true],
   ['xxxxxxxxxxxx', 'xxxxx', true],
   ['xxxxxxxxxxxx', 'xxxxx', true]
 ];
